34e5077d34b39936014a045ab62ce0dcc3cb128f
Lucas R. de Souza <LucasRSouza3500@gmail.com>
2026-07-20 11:28:50 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -4,5 +4,6 @@ export const connection = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     database: process.env.MYSQL_DATABASE,
-    password: process.env.MYSQLPASSWORD
+    password: process.env.MYSQLPASSWORD,
+    charset: 'utf8mb4'
 }).promise();
